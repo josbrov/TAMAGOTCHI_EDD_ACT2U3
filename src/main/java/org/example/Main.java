9ec284a028146d075eea3d2b73a1893465cda421
Jose Bertó <josbrov@gmail.com>
2026-01-03 10:42:31 +0100

    AÑADIDOS LOS ESTADOS DEL TAMAGOTCHI MEDIANTE EL METODO mostrarEstado()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
-
+static int saciedad=6;
+static int energia=6;
+static int diversion=6;
 public static void main(String[] args) {
 
     System.out.println("HOLA GIT");
     menuPrincipal();
+    mostrarEstado();
 
 }
 static String menu=("1. Estado\n" +
@@ -29,4 +32,30 @@ private static int menuPrincipal(){
        return opcion;
     }while (opcion>=0&&opcion<5);
 
+}
+
+private static void mostrarEstado() {
+    //1 el caso del game over para que se cumplanm los demas
+if(saciedad==0||energia==0||diversion==0){
+        System.out.println("(x_x) :( Game Over ):\n" +
+                "/|_|\\\n" +
+                "| |");
+
+}else if(saciedad>5&&energia>5&&diversion>5){
+    System.out.println("(•‿•) ¡Estoy feliz!\n" +
+            "/|_|\\\n" +
+            "| |");
+}else if(diversion<=4){
+    System.out.println("(-︵-) Estoy aburrido\n" +
+            "/|_|\\ ¡Juega conmigo!\n" +
+            "/ \\");
+}else if(energia<=4){
+    System.out.println("(-_-) Zzz\n" +
+            "/|_|\\\n" +
+            "| |");
+}else if(saciedad<=4){
+    System.out.println("(••︵) ¡ Tengo hambre!\uD83C\uDF57\n" +
+            "/|x|\\\n" +
+            "| |");
+}
 }
